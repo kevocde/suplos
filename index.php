60ff2fec34eac15bac58e1c11365fb46b1cea9cc
kevocde <kevocde@gmail.com>
2020-11-18 21:33:39 -0500

    Añadida cabecera

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link type="text/css" rel="stylesheet" href="css/index.css" media="screen,projection" />
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Formulario</title>
 </head>
 
